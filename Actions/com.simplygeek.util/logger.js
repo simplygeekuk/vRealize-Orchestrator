f1ce9369b4b2e7b90a7f0aab62704a0f0799b31e
nmshadey <gavin.stephens@simplygeek.co.uk>
2018-03-09 22:47:51 +0000

    Added code content

--- Actions/com.simplygeek.util/logger.js
+++ Actions/com.simplygeek.util/logger.js
@@ -1,1 +1,30 @@
+/*
+Purpose:
+This action is used to provide consistent logging output for the calling actions/workflows.
+This allows greater visibility into what code is executing and where, which makes troubleshooting easier.
+ 
+Parameters:
+– logType (string)
+– logName (string)
+– logLevel (string)
+– logMessage (Any)
+ 
+Return Type : void
+*/
 
+switch(logLevel) {
+    case "log":
+        System.log("[" + logType + ": " + logName + "]: " + logMessage);
+        break;
+    case "debug":
+        System.debug("[" + logType + ": " + logName + "]: " + logMessage);
+        break;
+    case "error":
+        System.error("[" + logType + ": " + logName + "]: " + logMessage);
+        break;
+    case "warn":
+        System.warn("[" + logType + ": " + logName + "]: " + logMessage);
+        break;
+    default:
+        System.log("[" + logType + ": " + logName + "]: " + logMessage);
+}
