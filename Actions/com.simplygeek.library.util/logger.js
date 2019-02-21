ff87a9039e57dd22afc6c0ffef9ca2ffd32f7669
Gavin Stephens <gavin.stephens@simplygeek.co.uk>
2019-02-21 22:26:30 +0000

    Updated logger.js with ESLint options

--- Actions/com.simplygeek.library.util/logger.js
+++ Actions/com.simplygeek.library.util/logger.js
@@ -1,32 +1,35 @@
+/*global System*/
+
 /**
  * This action creates a Logger object that can be used to standardise the log output in workflows.
  * This provides greater visibility into what code is executing and where, which makes troubleshooting easier.
+ * @function logger
  * @param {string} logType - The component type i.e. Action or Workflow.
  * @param {string} logName - The Action or Workflow name.
  * @returns {*} The Logger object.
  */
 
 function Logger(logType, logName) {
-    this.type = logType; 
+    this.type = logType;
     this.name = logName;
-    this.log = function (logMsg){		
+    this.log = function (logMsg) {
         System.log("[" + this.type + ": " + this.name + "] " + logMsg);
-    }
-    this.warn = function (warnMsg){		
+    };
+    this.warn = function (warnMsg) {
         System.warn("[" + this.type + ": " + this.name + "] " + warnMsg);
-    }
-    this.error = function (errMsg, exception){		
+    };
+    this.error = function (errMsg, exception) {
         System.error("[" + this.type + ": " + this.name + "] " + errMsg);
         if (exception) {
             throw "[" + this.type + ": " + this.name + "] " + errMsg + " " + exception;
         } else {
             throw errMsg;
         }
-    }
-    this.debug = function (debugMsg){		
+    };
+    this.debug = function (debugMsg) {
         System.debug("[" + this.type + ": " + this.name +"] " + debugMsg);
-    }
+    };
 
 }
 
-return Logger
+return Logger;
