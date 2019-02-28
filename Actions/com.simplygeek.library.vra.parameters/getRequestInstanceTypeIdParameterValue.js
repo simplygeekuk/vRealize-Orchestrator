d1e2f76719585aa0973c89e28488786508994de3
Gavin Stephens <gavin.stephens@simplygeek.co.uk>
2019-02-28 16:44:55 +0000

    Added com.simplygeek.library.vra.parameters

---
+++ Actions/com.simplygeek.library.vra.parameters/getRequestInstanceTypeIdParameterValue.js
@@ -0,0 +1,27 @@
+/**
+ * Returns the value for the parameter '__asd_requestInstanceTypeId'.
+ * @author Gavin Stephens <gavin.stephens@simplygeek.co.uk>
+ * @version 1.1.0
+ * @function getRequestInstanceTypeIdParameterValue
+ * @returns {string} Returns the parameter value.
+ */
+
+var logType = "Action";
+var logName = "getRequestInstanceTypeIdParameterValue"; // This must be set to the name of the action
+var Logger = System.getModule("com.simplygeek.library.util").logger(logType, logName);
+var log = new Logger(logType, logName);
+var parameter = "__asd_requestInstanceTypeId";
+var paramValue;
+
+try {
+    log.log("Getting value for parameter '" + parameter + "'");
+    paramValue = System.getModule("com.simplygeek.library.vro.workflow").getExecutionContextParameterValue(parameter);
+    if (!paramValue) {
+        log.error("No value was found for parameter '" + parameter + "'");
+    }
+    log.log("Found parameter value '" + paramValue + "'");
+} catch (e) {
+    log.error("Action failed to get value for parameter '" + parameter + "'",e);
+}
+
+return paramValue;
---
+++ Actions/com.simplygeek.library.vra.parameters/getSubtenantRefParameterValue.js
@@ -0,0 +1,27 @@
+/**
+ * Returns the value for the parameter '__asd_subtenantRef' (Business Group Id).
+ * @author Gavin Stephens <gavin.stephens@simplygeek.co.uk>
+ * @version 1.1.0
+ * @function getSubtenantRefParameterValue
+ * @returns {string} Returns the parameter value.
+ */
+
+var logType = "Action";
+var logName = "getSubtenantRefParameterValue"; // This must be set to the name of the action
+var Logger = System.getModule("com.simplygeek.library.util").logger(logType, logName);
+var log = new Logger(logType, logName);
+var parameter = "__asd_subtenantRef";
+var paramValue;
+
+try {
+    log.log("Getting value for parameter '" + parameter + "'");
+    paramValue = System.getModule("com.simplygeek.library.vro.workflow").getExecutionContextParameterValue(parameter);
+    if (!paramValue) {
+        log.error("No value was found for parameter '" + parameter + "'");
+    }
+    log.log("Found parameter value '" + paramValue + "'");
+} catch (e) {
+    log.error("Action failed to get value for parameter '" + parameter + "'",e);
+}
+
+return paramValue;
